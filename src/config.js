1f7880f7c17122e145cc5722a28b669cbbdae8fa
LAZKORIR <lazaruskorir95@gmail.com>
2025-02-03 11:29:25 -0600

    api url updated

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    apiBaseUrl: 'https://t86zva79uh.execute-api.us-east-1.amazonaws.com/dev',
+    apiBaseUrl: 'https://tat1xrq752.execute-api.us-east-1.amazonaws.com/prod',
 };
 
 export default config;
